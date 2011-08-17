2991a7334724f3ce98ff7168adb129484c464b3a
Wyclif Luyima <wyclif@openmrs.org>
2011-08-17 20:39:50 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22639 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/smartcontainer/smartData/handler/SmartMedicationHandler.java
+++ api/src/main/java/org/openmrs/module/smartcontainer/smartData/handler/SmartMedicationHandler.java
@@ -7,61 +7,54 @@ import org.openmrs.DrugOrder;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.smartcontainer.SmartConceptMap;
-import org.openmrs.module.smartcontainer.smartData.SmartBaseData;
 import org.openmrs.module.smartcontainer.smartData.SmartMedication;
 import org.openmrs.module.smartcontainer.util.SmartDataHandlerUtil;
 
 public class SmartMedicationHandler implements SmartDataHandler<SmartMedication> {
+	
 	private SmartConceptMap map;
-
+	
 	public List<SmartMedication> getAllForPatient(Patient patient) {
-		List<DrugOrder> drugs = Context.getOrderService()
-				.getDrugOrdersByPatient(patient);
+		List<DrugOrder> drugs = Context.getOrderService().getDrugOrdersByPatient(patient);
 		List<SmartMedication> medications = new ArrayList<SmartMedication>();
 		for (DrugOrder d : drugs) {
 			SmartMedication medication = new SmartMedication();
-			medication.setDrugName(SmartDataHandlerUtil.codedValueHelper(d
-					.getDrug().getConcept(), getMap()));
+			medication.setDrugName(SmartDataHandlerUtil.codedValueHelper(d.getDrug().getConcept(), getMap()));
 			
 			if (d.getAutoExpireDate() != null)// may be not expired yet
-				medication.setEndDate(SmartDataHandlerUtil.date(d
-						.getAutoExpireDate()));
+				medication.setEndDate(SmartDataHandlerUtil.date(d.getAutoExpireDate()));
 			
 			// TODO: what if it isn't started yet?  Is startDate allowed to be null?
-			medication
-					.setStartDate(SmartDataHandlerUtil.date(d.getStartDate()));
-
+			medication.setStartDate(SmartDataHandlerUtil.date(d.getStartDate()));
+			
 			// for quantity
-			medication.setQuantity(SmartDataHandlerUtil.valueAndUnitHelper(
-					d.getQuantity(), d.getUnits()));
+			medication.setQuantity(SmartDataHandlerUtil.valueAndUnitHelper(d.getQuantity(), d.getUnits()));
 			
 			// for frequency
 			String frequency = d.getFrequency().split(" ")[0];
 			Integer val = Integer.valueOf(frequency.split("/")[0]);
 			
-			
 			// TODO: why are we presuming per day here ?!
 			
-			medication.setFrequency(SmartDataHandlerUtil.valueAndUnitHelper(
-					val, "/d"));
+			medication.setFrequency(SmartDataHandlerUtil.valueAndUnitHelper(val, "/d"));
 			
 			// TODO:if the instruction is not present generate one
 			medication.setInstructions(d.getInstructions());
 			//
 			medications.add(medication);
-
+			
 		}
 		return medications;
 	}
-
+	
 	public SmartConceptMap getMap() {
 		return map;
 	}
-
+	
 	public void setMap(SmartConceptMap map) {
 		this.map = map;
 	}
-
+	
 	public SmartMedication getForPatient(Patient patient) {
 		
 		return null;
