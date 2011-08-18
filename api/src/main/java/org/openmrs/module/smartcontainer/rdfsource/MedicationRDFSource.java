e4f2c0e6b9fe3c436c89b8b8f6fbebdd5a247c23
Wyclif Luyima <wyclif@openmrs.org>
2011-08-18 00:22:27 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22651 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/smartcontainer/rdfsource/MedicationRDFSource.java
+++ api/src/main/java/org/openmrs/module/smartcontainer/rdfsource/MedicationRDFSource.java
@@ -1,5 +1,10 @@
 package org.openmrs.module.smartcontainer.rdfsource;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.List;
+
 import org.openmrs.module.smartcontainer.RdfSource;
 import org.openmrs.module.smartcontainer.smartData.SmartMedication;
 import org.openmrs.module.smartcontainer.util.RdfUtil;
@@ -9,82 +14,77 @@ import org.openrdf.model.Value;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.rdfxml.util.RDFXMLPrettyWriter;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.List;
-
 /**
  * Render a RDF/XML for SMART Medication
  */
 public class MedicationRDFSource extends RdfSource {
-
-    /**
-     * Primary method to render
-     *
-     * @param meds
-     * @return
-     * @throws IOException
-     */
-    public String getRDF(List<SmartMedication> meds) throws IOException, RDFHandlerException {
-        Writer sWriter = new StringWriter();
-        RDFXMLPrettyWriter graph = new RDFXMLPrettyWriter(sWriter);
-
-        addHeader(graph);
-        graph.startRDF();
-        for (SmartMedication med : meds) {
-            /*
-                * Add parent node <sp:Medication> ..child nodes </sp:Medication>
-                */
-            BNode medicationNode = factory.createBNode();
-            URI medication = factory.createURI(sp, "Medication");
-
-            graph.handleStatement(factory.createStatement(medicationNode, RdfSource.type, medication));
-            /*
-                * Add child node <sp:drugName> ..coded value node </sp:drugName>
-                */
-            URI drugName = factory.createURI(sp, "drugName");
-            graph.handleStatement(factory.createStatement(medicationNode, drugName,
-                    RdfUtil.codedValue(factory, graph, med.getDrugName())));
-            /*
-                * Add child node <sp:startDate>2007-03-14</sp:startDate>
-                */
-            if (med.getStartDate() != null) {
-                URI startDate = factory.createURI(sp, "startDate");
-                Value startDateVal = factory.createLiteral(med.getStartDate());
-                graph.handleStatement(factory.createStatement(medicationNode, startDate, startDateVal));
-            }
-            /*
-                * Add child node <sp:instructions>Take two tablets twice daily as
-                * needed for pain</sp:instructions>
-                */
-            if (med.getInstructions() != null) {
-                URI instructions = factory.createURI(sp, "instructions");
-                Value sig = factory.createLiteral(med.getInstructions());
-                graph.handleStatement(factory.createStatement(medicationNode, instructions, sig));
-            }
-            /*
-                * Add child node <sp:quantity> ..Value and unit node </sp:quantity>
-                */
-            if ((med.getQuantity() != null)) {
-                URI quantity = factory.createURI(sp, "quantity");
-                graph.handleStatement(factory.createStatement(medicationNode, quantity,
-                        RdfUtil.valueAndUnit(factory, graph, med.getQuantity())));
-            }
-            /*
-                * Add child node <sp:frequency> ..Value and unit node
-                * </sp:frequency>
-                */
-            if ((med.getFrequency() != null)) {
-                URI frequency = factory.createURI(sp, "frequency");
-                graph.handleStatement(factory.createStatement(medicationNode, frequency, RdfUtil
-                        .valueAndUnit(factory, graph, med.getFrequency())));
-
-            }//
-
-        }
-        graph.endRDF();
-        return sWriter.toString();
-    }
-
+	
+	/**
+	 * Primary method to render
+	 * 
+	 * @param meds
+	 * @return
+	 * @throws IOException
+	 */
+	public String getRDF(List<SmartMedication> meds) throws IOException, RDFHandlerException {
+		Writer sWriter = new StringWriter();
+		RDFXMLPrettyWriter graph = new RDFXMLPrettyWriter(sWriter);
+		
+		addHeader(graph);
+		graph.startRDF();
+		for (SmartMedication med : meds) {
+			/*
+			    * Add parent node <sp:Medication> ..child nodes </sp:Medication>
+			    */
+			BNode medicationNode = factory.createBNode();
+			URI medication = factory.createURI(sp, "Medication");
+			
+			graph.handleStatement(factory.createStatement(medicationNode, RdfSource.type, medication));
+			/*
+			    * Add child node <sp:drugName> ..coded value node </sp:drugName>
+			    */
+			URI drugName = factory.createURI(sp, "drugName");
+			graph.handleStatement(factory.createStatement(medicationNode, drugName,
+			    RdfUtil.codedValue(factory, graph, med.getDrugName())));
+			/*
+			    * Add child node <sp:startDate>2007-03-14</sp:startDate>
+			    */
+			if (med.getStartDate() != null) {
+				URI startDate = factory.createURI(sp, "startDate");
+				Value startDateVal = factory.createLiteral(med.getStartDate());
+				graph.handleStatement(factory.createStatement(medicationNode, startDate, startDateVal));
+			}
+			/*
+			    * Add child node <sp:instructions>Take two tablets twice daily as
+			    * needed for pain</sp:instructions>
+			    */
+			if (med.getInstructions() != null) {
+				URI instructions = factory.createURI(sp, "instructions");
+				Value sig = factory.createLiteral(med.getInstructions());
+				graph.handleStatement(factory.createStatement(medicationNode, instructions, sig));
+			}
+			/*
+			    * Add child node <sp:quantity> ..Value and unit node </sp:quantity>
+			    */
+			if ((med.getQuantity() != null)) {
+				URI quantity = factory.createURI(sp, "quantity");
+				graph.handleStatement(factory.createStatement(medicationNode, quantity,
+				    RdfUtil.valueAndUnit(factory, graph, med.getQuantity())));
+			}
+			/*
+			    * Add child node <sp:frequency> ..Value and unit node
+			    * </sp:frequency>
+			    */
+			if ((med.getFrequency() != null)) {
+				URI frequency = factory.createURI(sp, "frequency");
+				graph.handleStatement(factory.createStatement(medicationNode, frequency,
+				    RdfUtil.valueAndUnit(factory, graph, med.getFrequency())));
+				
+			}//
+			
+		}
+		graph.endRDF();
+		return sWriter.toString();
+	}
+	
 }
