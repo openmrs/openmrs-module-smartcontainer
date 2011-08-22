dbe4808d2cccc9f49f07b50c306def3644bd92da
Wyclif Luyima <wyclif@openmrs.org>
2011-08-22 22:08:10 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22828 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- omod/src/main/java/org/openmrs/module/smartcontainer/web/controller/ManageAppUserlevelController.java
+++ omod/src/main/java/org/openmrs/module/smartcontainer/web/controller/ManageAppUserlevelController.java
@@ -1,5 +1,11 @@
 package org.openmrs.module.smartcontainer.web.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
@@ -12,109 +18,108 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Set;
-
 /**
  * @author aja
  */
 @Controller
 @RequestMapping(value = "module/smartcontainer/manageUserAppLink.form")
 public class ManageAppUserlevelController {
-    Log log = LogFactory.getLog(getClass());
-
-    private SmartAppService service;
-    private final String SUCCESS_FORM_VIEW = "/module/smartcontainer/addAppUserLevel";
-
-    /**
-     * Initially called after the formBackingObject method to get the landing form name
-     *
-     * @return String form view name
-     */
-    @RequestMapping(method = RequestMethod.GET)
-    public String showForm() {
-        return SUCCESS_FORM_VIEW;
-
-    }
-
-    /**
-     * All the parameters are optional based on the necessity
-     *
-     * @param request
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST)
-    public String deleteApp(HttpServletRequest request) {
-        SmartUser smartUser = Context.getService(SmartUserService.class).getUserByName(Context.getAuthenticatedUser().getSystemId());
-        Collection<App> allApps = service.getAllApps();
-        Collection<App> userApps = smartUser.getApps();
-        if (userApps == null) {
-            userApps = new ArrayList<App>();
-        }
-
-        String add[] = request.getParameterValues("add");
-        String remove[] = request.getParameterValues("remove");
-        log.debug("saving users........... add " + add + "remove :" + remove);
-        if (add != null) {
-            for (String app : add) {
-                for (App a : allApps) {
-                    if (a.getName().equals(app)) {
-                        userApps.add(a);
-                    }
-                }
-
-            }
-        }
-        if (remove != null) {
-            for (String app : remove) {
-                for (App a : allApps) {
-                    if (a.getName().equals(app)) {
-                        userApps.remove(a);
-                    }
-                }
-
-            }
-        }
-        SmartUser user = Context.getService(SmartUserService.class)
-                .getUserByName(Context.getAuthenticatedUser().getSystemId());
-        user.setApps((Set<App>) userApps);
-        Context.getService(SmartUserService.class).saveUser(user);
-        log.debug("saved user........... add " + user.getOpenMRSUser().getSystemId());
-        return SUCCESS_FORM_VIEW;
-
-    }
-
-    /**
-     * This class returns the form backing object. This can be a string, a boolean, or a normal java
-     * pojo. The bean name defined in the ModelAttribute annotation and the type can be just defined
-     * by the return type of this method
-     */
-    @ModelAttribute("userApps")
-    protected Collection<App> formBackingObject(HttpServletRequest request) throws Exception {
-        service = Context.getService(SmartAppService.class);
-        SmartUser smartUser = null;
-        Collection<App> allApps = service.getAllApps();
-        Collection<App> userApps = null;
-
-
-        try {
-            smartUser = Context.getService(SmartUserService.class).getUserByName(Context.getAuthenticatedUser().getSystemId());
-        } catch (IndexOutOfBoundsException ex) {
-            if (smartUser == null) {
-                smartUser = new SmartUser();
-                smartUser.setOpenMRSUser(Context.getAuthenticatedUser());
-                Context.getService(SmartUserService.class).saveUser(smartUser);
-
-            }
-        }
-        userApps = smartUser.getApps();
-        if (userApps == null) {
-            userApps = new ArrayList<App>();
-        }
-        //request.setAttribute("userApps", userApps);
-        request.setAttribute("allApps", allApps);
-        return userApps;
-    }
+	
+	Log log = LogFactory.getLog(getClass());
+	
+	private SmartAppService service;
+	
+	private final String SUCCESS_FORM_VIEW = "/module/smartcontainer/addAppUserLevel";
+	
+	/**
+	 * Initially called after the formBackingObject method to get the landing form name
+	 * 
+	 * @return String form view name
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public String showForm() {
+		return SUCCESS_FORM_VIEW;
+		
+	}
+	
+	/**
+	 * All the parameters are optional based on the necessity
+	 * 
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public String deleteApp(HttpServletRequest request) {
+		SmartUser smartUser = Context.getService(SmartUserService.class).getUserByName(
+		    Context.getAuthenticatedUser().getSystemId());
+		Collection<App> allApps = service.getAllApps();
+		Collection<App> userApps = smartUser.getApps();
+		if (userApps == null) {
+			userApps = new ArrayList<App>();
+		}
+		
+		String add[] = request.getParameterValues("add");
+		String remove[] = request.getParameterValues("remove");
+		log.debug("saving users........... add " + add + "remove :" + remove);
+		if (add != null) {
+			for (String app : add) {
+				for (App a : allApps) {
+					if (a.getName().equals(app)) {
+						userApps.add(a);
+					}
+				}
+				
+			}
+		}
+		if (remove != null) {
+			for (String app : remove) {
+				for (App a : allApps) {
+					if (a.getName().equals(app)) {
+						userApps.remove(a);
+					}
+				}
+				
+			}
+		}
+		SmartUser user = Context.getService(SmartUserService.class).getUserByName(
+		    Context.getAuthenticatedUser().getSystemId());
+		user.setApps((Set<App>) userApps);
+		Context.getService(SmartUserService.class).saveUser(user);
+		log.debug("saved user........... add " + user.getOpenMRSUser().getSystemId());
+		return SUCCESS_FORM_VIEW;
+		
+	}
+	
+	/**
+	 * This class returns the form backing object. This can be a string, a boolean, or a normal java
+	 * pojo. The bean name defined in the ModelAttribute annotation and the type can be just defined
+	 * by the return type of this method
+	 */
+	@ModelAttribute("userApps")
+	protected Collection<App> formBackingObject(HttpServletRequest request) throws Exception {
+		service = Context.getService(SmartAppService.class);
+		SmartUser smartUser = null;
+		Collection<App> allApps = service.getAllApps();
+		Collection<App> userApps = null;
+		
+		try {
+			smartUser = Context.getService(SmartUserService.class).getUserByName(
+			    Context.getAuthenticatedUser().getSystemId());
+		}
+		catch (IndexOutOfBoundsException ex) {
+			if (smartUser == null) {
+				smartUser = new SmartUser();
+				smartUser.setOpenMRSUser(Context.getAuthenticatedUser());
+				Context.getService(SmartUserService.class).saveUser(smartUser);
+				
+			}
+		}
+		userApps = smartUser.getApps();
+		if (userApps == null) {
+			userApps = new ArrayList<App>();
+		}
+		//request.setAttribute("userApps", userApps);
+		request.setAttribute("allApps", allApps);
+		return userApps;
+	}
 }
