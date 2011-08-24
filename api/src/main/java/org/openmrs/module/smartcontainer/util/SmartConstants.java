1eb88f2d1e0ca6d39c578e5e06ae31100956c830
Wyclif Luyima <wyclif@openmrs.org>
2011-08-24 04:31:08 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22854 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/smartcontainer/util/SmartConstants.java
+++ api/src/main/java/org/openmrs/module/smartcontainer/util/SmartConstants.java
@@ -17,48 +17,44 @@ package org.openmrs.module.smartcontainer.util;
  * Constants for the smartcontainer module
  */
 public class SmartConstants {
-
+	
 	/**
-	 * The name of the global property to store the true/false value of whether
-	 * this instance wants to use the ActiveList/Problem table and object to
-	 * manage the patient's problems <br/>
-	 * Also see the config.xml for the default inclusion of this property in a
-	 * user's db
+	 * The name of the global property to store the true/false value of whether this instance wants
+	 * to use the ActiveList/Problem table and object to manage the patient's problems <br/>
+	 * Also see the config.xml for the default inclusion of this property in a user's db
 	 */
 	public static final String GP_USE_PROBLEM_OBJECT_NAME = "smartcontainer.problem.useProblemObject";
-
+	
 	/**
-	 * The name of the global property to store the true/false of whether this
-	 * instance wants to use the obs table to look up a patient's problems <br/>
-	 * Also see the config.xml for the default inclusion of this property in a
-	 * user's db
+	 * The name of the global property to store the true/false of whether this instance wants to use
+	 * the obs table to look up a patient's problems <br/>
+	 * Also see the config.xml for the default inclusion of this property in a user's db
 	 * 
 	 * @see SmartConstants#GP_PROBLEM_ADDED_CONCEPT
 	 * @see SmartConstants#GP_PROBLEM_RESOLVED_CONCEPT
 	 */
 	public static final String GP_USE_OBS_FOR_PROBLEM = "smartcontainer.problem.useObsForProblem";
-
+	
 	/**
-	 * The name of the global property to store the concept id of the "Problem Added" concept 
-	 * used in forms.  This can be used in place or in addition to the problem object
+	 * The name of the global property to store the concept id of the "Problem Added" concept used
+	 * in forms. This can be used in place or in addition to the problem object
 	 * 
 	 * @see #GP_USE_OBS_FOR_PROBLEM
 	 */
 	public static String GP_PROBLEM_ADDED_CONCEPT = "smartcontainer.problem.addedConcept";
 	
 	/**
-	 * The name of the global property to store the concept id of the "Problem Resolved" concept 
-	 * used in forms.  This can be used in place or in addition to the problem object
+	 * The name of the global property to store the concept id of the "Problem Resolved" concept
+	 * used in forms. This can be used in place or in addition to the problem object
 	 * 
 	 * @see #GP_USE_OBS_FOR_PROBLEM
 	 */
 	public static String GP_PROBLEM_RESOLVED_CONCEPT = "smartcontainer.problem.resolvedConcept";
-
+	
 	/**
-	 * The name of the global property to store the true/false value of whether
-	 * this instance wants to use the ActiveList/Allergy table and object to
-	 * manage the patient's problems
+	 * The name of the global property to store the true/false value of whether this instance wants
+	 * to use the ActiveList/Allergy table and object to manage the patient's problems
 	 */
 	public static final String GP_USE_ALLERGY_OBJECT = "smartcontainer.allergy.useAllergyObject";
-
+	
 }
