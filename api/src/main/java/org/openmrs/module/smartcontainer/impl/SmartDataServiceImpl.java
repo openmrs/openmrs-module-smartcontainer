c09f17929a85b81e476f82a9966ad822dab99c46
Wyclif Luyima <wyclif@openmrs.org>
2011-08-26 18:46:35 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22934 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/smartcontainer/impl/SmartDataServiceImpl.java
+++ api/src/main/java/org/openmrs/module/smartcontainer/impl/SmartDataServiceImpl.java
@@ -13,63 +13,62 @@
  */
 package org.openmrs.module.smartcontainer.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import org.openmrs.Patient;
 import org.openmrs.module.smartcontainer.SmartDataService;
 import org.openmrs.module.smartcontainer.smartData.SmartBaseData;
 import org.openmrs.module.smartcontainer.smartData.handler.SmartDataHandler;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * Actual implementation of SmartDataService
  */
 public class SmartDataServiceImpl implements SmartDataService {
-    private Map<String, SmartDataHandler<? extends SmartBaseData>> handlers;
-
-    public Map<String, SmartDataHandler<? extends SmartBaseData>> getHandlers() {
-        return handlers;
-    }
-
-    /**
-     * This setter acts more like an "appender". If the list already has
-     * elements, calling this method will add to the list of handlers instead of
-     * replacing it.
-     *
-     * @param h
-     */
-    public void setHandlers(Map<String, SmartDataHandler<? extends SmartBaseData>> h) {
-        if (this.handlers == null) {
-            this.handlers = h;
-        } else {
-            for (String s : h.keySet()) {
-                if (!handlers.containsKey(s)) {
-                    handlers.put(s, h.get(s));
-                }
-            }
-        }
-    }
-
-    /**
-     * @see org.openmrs.module.smartcontainer.SmartDataService#getAllForPatient(org.openmrs.Patient,
-     *      java.lang.Class)
-     */
-    public <T extends SmartBaseData> List<T> getAllForPatient(Patient patient,
-                                                              Class<T> clazz) {
-        // Get a handler from the handlers which is mapped to simple name of the
-        // clazz and call getAllForPatient(patient)
-        return (List<T>) handlers.get(clazz.getSimpleName()).getAllForPatient(patient);
-    }
-
-    /**
-     * @see org.openmrs.module.smartcontainer.SmartDataService#getForPatient(org.openmrs.Patient,
-     *      java.lang.Class)
-     */
-    public <T extends SmartBaseData> T getForPatient(Patient patient, Class<T> clazz) {
-        // Get a handler from the handlers which is mapped to simple name of the
-        // clazz and call getForPatient(patient)
-        return (T) handlers.get(clazz.getSimpleName()).getForPatient(patient);
-
-    }
-
+	
+	private Map<String, SmartDataHandler<? extends SmartBaseData>> handlers;
+	
+	public Map<String, SmartDataHandler<? extends SmartBaseData>> getHandlers() {
+		return handlers;
+	}
+	
+	/**
+	 * This setter acts more like an "appender". If the list already has elements, calling this
+	 * method will add to the list of handlers instead of replacing it.
+	 * 
+	 * @param h
+	 */
+	public void setHandlers(Map<String, SmartDataHandler<? extends SmartBaseData>> h) {
+		if (this.handlers == null) {
+			this.handlers = h;
+		} else {
+			for (String s : h.keySet()) {
+				if (!handlers.containsKey(s)) {
+					handlers.put(s, h.get(s));
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @see org.openmrs.module.smartcontainer.SmartDataService#getAllForPatient(org.openmrs.Patient,
+	 *      java.lang.Class)
+	 */
+	public <T extends SmartBaseData> List<T> getAllForPatient(Patient patient, Class<T> clazz) {
+		// Get a handler from the handlers which is mapped to simple name of the
+		// clazz and call getAllForPatient(patient)
+		return (List<T>) handlers.get(clazz.getSimpleName()).getAllForPatient(patient);
+	}
+	
+	/**
+	 * @see org.openmrs.module.smartcontainer.SmartDataService#getForPatient(org.openmrs.Patient,
+	 *      java.lang.Class)
+	 */
+	public <T extends SmartBaseData> T getForPatient(Patient patient, Class<T> clazz) {
+		// Get a handler from the handlers which is mapped to simple name of the
+		// clazz and call getForPatient(patient)
+		return (T) handlers.get(clazz.getSimpleName()).getForPatient(patient);
+		
+	}
+	
 }
