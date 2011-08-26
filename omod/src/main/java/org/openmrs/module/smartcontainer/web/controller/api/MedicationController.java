5b67c2fe28e5c6cb67e53e297a9300cbd8ea302e
Wyclif Luyima <wyclif@openmrs.org>
2011-08-26 22:10:21 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22937 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- omod/src/main/java/org/openmrs/module/smartcontainer/web/controller/api/MedicationController.java
+++ omod/src/main/java/org/openmrs/module/smartcontainer/web/controller/api/MedicationController.java
@@ -1,5 +1,11 @@
 package org.openmrs.module.smartcontainer.web.controller.api;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Patient;
@@ -14,47 +20,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.List;
-
 @Controller
 @RequestMapping(value = "/smartcontainer/api/")
 public class MedicationController {
-
-    Log log = LogFactory.getLog(getClass());
-
-    private MedicationRDFSource resource;
-
-    public MedicationRDFSource getResource() {
-        return resource;
-    }
-
-    public void setResource(MedicationRDFSource resource) {
-        this.resource = resource;
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "records/{pid}/medications/")
-    public ModelAndView handle(@PathVariable("pid") Patient patient,
-                               HttpServletResponse resp) {
-        log.info("In the Medication Controller");
-        resp.setContentType("text/xml"); // actually I use a constant
-        Writer writer;
-        try {
-            writer = resp.getWriter();
-            List<SmartMedication> meds = Context.getService(
-                    SmartDataService.class).getAllForPatient(patient,
-                    SmartMedication.class);
-            writer.write(resource.getRDF(meds)); // get the object
-            writer.close();
-        } catch (IOException e) {
-
-            log.error("Unable to write out Medication for pid: " + patient.getId(), e);
-        } catch (RDFHandlerException e) {
-            log.error("Unable to write out Medication for pid: " + patient.getId(), e);
-        }
-        return null; // indicates this controller did all necessary processing
-
-    }
+	
+	Log log = LogFactory.getLog(getClass());
+	
+	private MedicationRDFSource resource;
+	
+	public MedicationRDFSource getResource() {
+		return resource;
+	}
+	
+	public void setResource(MedicationRDFSource resource) {
+		this.resource = resource;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "records/{pid}/medications/")
+	public ModelAndView handle(@PathVariable("pid") Patient patient, HttpServletResponse resp) {
+		log.info("In the Medication Controller");
+		resp.setContentType("text/xml"); // actually I use a constant
+		Writer writer;
+		try {
+			writer = resp.getWriter();
+			List<SmartMedication> meds = Context.getService(SmartDataService.class).getAllForPatient(patient,
+			    SmartMedication.class);
+			writer.write(resource.getRDF(meds)); // get the object
+			writer.close();
+		}
+		catch (IOException e) {
+			
+			log.error("Unable to write out Medication for pid: " + patient.getId(), e);
+		}
+		catch (RDFHandlerException e) {
+			log.error("Unable to write out Medication for pid: " + patient.getId(), e);
+		}
+		return null; // indicates this controller did all necessary processing
+		
+	}
 }
