ed80feed41d10ad4672359f77c19364b53373cdf
Wyclif Luyima <wyclif@openmrs.org>
2011-08-27 00:43:18 +0000

    Applying auto formatting
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@22940 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/smartcontainer/db/hibernate/HibernateUserDAO.java
+++ api/src/main/java/org/openmrs/module/smartcontainer/db/hibernate/HibernateUserDAO.java
@@ -13,6 +13,9 @@
  */
 package org.openmrs.module.smartcontainer.db.hibernate;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.openmrs.api.APIException;
@@ -20,60 +23,57 @@ import org.openmrs.api.db.DAOException;
 import org.openmrs.module.smartcontainer.SmartUser;
 import org.openmrs.module.smartcontainer.db.UserDAO;
 
-import java.util.Collection;
-import java.util.List;
-
 /**
  * Implementation for User DAO
  */
 public class HibernateUserDAO implements UserDAO {
-
-    private SessionFactory sessionFactory;
-
-    /**
-     * @return the sessionFactory
-     */
-    public SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-    /**
-     * @param sessionFactory the sessionFactory to set
-     */
-    public void setSessionFactory(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
-    /**
-     * @see org.openmrs.module.smartcontainer.db.UserDAO#getUserByName(java.lang.String)
-     */
-    public SmartUser getUserByName(String name) throws DAOException {
-        Query query = sessionFactory
-                .getCurrentSession()
-                .createQuery(
-                        "from SmartUser u where u.openMRSUser.retired = 0 and (u.openMRSUser.username = ? or u.openMRSUser.systemId = ?)");
-        query.setString(0, name);
-        query.setString(1, name);
-        List<SmartUser> users = query.list();
-
-        return users.get(0);
-    }
-
-    /**
-     * @see org.openmrs.module.smartcontainer.db.UserDAO#getAllUsers()
-     */
-    public Collection<SmartUser> getAllUsers() throws DAOException {
-
-        return sessionFactory.getCurrentSession().createQuery("from SmartUser u order by u.openMRSUser.userId").list();
-    }
-
-    /**
-     * @see org.openmrs.module.smartcontainer.db.UserDAO#saveUser(org.openmrs.module.smartcontainer.SmartUser)
-     */
-    public void saveUser(SmartUser user) throws APIException {
-
-        sessionFactory.getCurrentSession().saveOrUpdate(user);
-
-    }
-
+	
+	private SessionFactory sessionFactory;
+	
+	/**
+	 * @return the sessionFactory
+	 */
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	/**
+	 * @param sessionFactory the sessionFactory to set
+	 */
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	/**
+	 * @see org.openmrs.module.smartcontainer.db.UserDAO#getUserByName(java.lang.String)
+	 */
+	public SmartUser getUserByName(String name) throws DAOException {
+		Query query = sessionFactory
+		        .getCurrentSession()
+		        .createQuery(
+		            "from SmartUser u where u.openMRSUser.retired = 0 and (u.openMRSUser.username = ? or u.openMRSUser.systemId = ?)");
+		query.setString(0, name);
+		query.setString(1, name);
+		List<SmartUser> users = query.list();
+		
+		return users.get(0);
+	}
+	
+	/**
+	 * @see org.openmrs.module.smartcontainer.db.UserDAO#getAllUsers()
+	 */
+	public Collection<SmartUser> getAllUsers() throws DAOException {
+		
+		return sessionFactory.getCurrentSession().createQuery("from SmartUser u order by u.openMRSUser.userId").list();
+	}
+	
+	/**
+	 * @see org.openmrs.module.smartcontainer.db.UserDAO#saveUser(org.openmrs.module.smartcontainer.SmartUser)
+	 */
+	public void saveUser(SmartUser user) throws APIException {
+		
+		sessionFactory.getCurrentSession().saveOrUpdate(user);
+		
+	}
+	
 }
