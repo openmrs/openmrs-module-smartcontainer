0a3d9d267691c0a1488a640dbaf42b51ffc6c53e
Wyclif Luyima <wyclif@openmrs.org>
2012-01-26 00:21:06 +0000

    Applying auto format changes
    git-svn-id: http://svn.openmrs.org/openmrs-modules/smartcontainer/trunk@25551 5bac5841-c719-aa4e-b3fe-cce5062f897a

--- api/src/main/java/org/openmrs/module/smartcontainer/smartData/SmartDemographics.java
+++ api/src/main/java/org/openmrs/module/smartcontainer/smartData/SmartDemographics.java
@@ -3,62 +3,66 @@ package org.openmrs.module.smartcontainer.smartData;
 /**
  * The class representing SMART datatype
  * <p/>
- * <a
- * href="http://wiki.chip.org/smart-project/index.php/Developers_Documentation:_SMART_Data_Model#Demographics_RDF">
- * Demographics</a>
+ * <a href=
+ * "http://wiki.chip.org/smart-project/index.php/Developers_Documentation:_SMART_Data_Model#Demographics_RDF"
+ * > Demographics</a>
  */
 public class SmartDemographics extends BaseSmartData {
-    private String givenName;
-    private String familyName;
-    private String gender;
-    private String zipCode;
-    private String birthDate;
-
-    public String getGivenName() {
-        return givenName;
-    }
-
-    public void setGivenName(String givenName) {
-        this.givenName = givenName;
-    }
-
-    public String getFamilyName() {
-        return familyName;
-    }
-
-    public void setFamilyName(String familyName) {
-        this.familyName = familyName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    @Override
-    public String toString() {
-        return "SmartDemographics [givenName=" + givenName + ", familyName="
-                + familyName + ", gender=" + gender + ", zipCode=" + zipCode
-                + ", birthDate=" + birthDate + "]";
-    }
-
+	
+	private String givenName;
+	
+	private String familyName;
+	
+	private String gender;
+	
+	private String zipCode;
+	
+	private String birthDate;
+	
+	public String getGivenName() {
+		return givenName;
+	}
+	
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	
+	public String getFamilyName() {
+		return familyName;
+	}
+	
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getZipCode() {
+		return zipCode;
+	}
+	
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	public String getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "SmartDemographics [givenName=" + givenName + ", familyName=" + familyName + ", gender=" + gender
+		        + ", zipCode=" + zipCode + ", birthDate=" + birthDate + "]";
+	}
+	
 }
